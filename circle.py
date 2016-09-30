3f52ec447e07ca35719a0e9c953a61e7e3437b83
homomaid <canon.xex@gmail.com>
2016-09-30 18:48:13 +0900

    commit

--- circle.py
+++ circle.py
@@ -1,40 +1,33 @@
-import cv2
+#coding: utf-8
+
 import numpy as np
+import cv2
 
-
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+capture = cv2.VideoCapture(2)
+capture.set(3, 640)
+capture.set(4, 480)
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    preprocessed = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    cv2.imshow('camera capture', frame)
+    result = cv2.HoughCircles(image = preprocessed, method = cv2.HOUGH_GRADIENT, dp=1.5,
+                              param1 = 100, param2 = 110,
+                              minDist = 100, minRadius = 40, maxRadius =250)
 
-    circles = cv2.HoughCircles(image = gray, method = cv2.HOUGH_GRADIENT, \
-                               dp = 1, minDist = 20, minRadius = 20, maxRadius = 200)
+    if result is not None and len(result) > 0:
+        circles = result[0]
+        for(x, y, r) in circles:
+            print(x, y, r)
+            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
 
-    #circles = np.uint16(np.around(circles))
-
-    for i in circles:
-        cv2.circle(frame, (i[0], i[1]), i[2], (0,255,0), 2)
-
-    #if circles is not None and len(circles) > 0:
-    #    for(x, y, r) in circles:
-    #        print(x, y, r)
-    #        x, y, r = int(x), int(y), int(r)
-    #        cv2.circle(img = frame, center = (x, y), radius = r, color = (255, 0, 0))
-    #elif circles is None:
-    #    print("if 文が意味をなしていないぞ")
-
+    cv2.imshow('ball', frame)
     k = cv2.waitKey(1)
     if k == 27:
         break
 
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
-
-
 
