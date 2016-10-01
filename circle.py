dd7ea1fad732c4231835e5c9285d6eceffa861ee
homomaid <canon.xex@gmail.com>
2016-10-01 19:50:15 +0900

    commit

--- circle.py
+++ circle.py
@@ -2,27 +2,22 @@
 
 import cv2
 
-capture = cv2.VideoCapture(2)
-capture.set(3, 640)
-capture.set(4, 480)
+capture = cv2.VideoCapture(0)
+
+_, frame = capture.read()
+roi = cv2.selectROI('tracker', frame, False, False)
+
+tracker = cv2.Tracker_create("KCF")
+tracker.init(frame, roi)
 
 while True:
     ret, frame = capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    preprocessed = cv2.GaussianBlur(gray, (7, 7), 0)
-
-    result = cv2.HoughCircles(image=preprocessed, method=cv2.HOUGH_GRADIENT,
-                              dp=1.5, param1=100, param2=110,
-                              minDist=100, minRadius=40, maxRadius=250)
-
-    if result is not None and len(result) > 0:
-        circles = result[0]
-        for(x, y, r) in circles:
-            print(x, y, r)
-            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-
-    cv2.imshow('ball', frame)
+    _, position = tracker.update(frame)
+    position = list(map(int, position))
+    cv2.rectangle(frame, (position[0], position[1]),
+                  (position[0] + position[2], position[1] + position[3]),
+                  (0, 255, 0))
+    cv2.imshow('tracker', frame)
     k = cv2.waitKey(1)
     if k == 27:
         break
